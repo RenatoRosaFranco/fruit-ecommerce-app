7919cf0568d9c6c89b153ba375c4dc3e86f4cd90
Renato Rosa Franco <renato_ny@live.com>
2023-04-28 20:10:45 -0300

    Initial commit

---
+++ lib/src/sign_in_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green,
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.green,
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(45)
+                )
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
