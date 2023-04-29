b99265dfb6673cb607fb64a1dd3112872f8ba3ba
Renato Rosa Franco <renato_ny@live.com>
2023-04-29 14:59:15 -0300

    Adicionando Botão de carrinho e formato de preço

--- lib/src/pages/home/components/item_tile.dart
+++ lib/src/pages/home/components/item_tile.dart
@@ -1,55 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:untitled4/src/config/custom_colors.dart';
 import 'package:untitled4/src/models/item_model.dart';
+import 'package:untitled4/src/services/utils_services.dart';
 
 class ItemTile extends StatelessWidget {
-  const ItemTile({Key? key, required this.item}) : super(key: key);
+  ItemTile({Key? key, required this.item}) : super(key: key);
 
   final ItemModel item;
+  final UtilsServices utilsServices = UtilsServices();
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 1,
-      shadowColor: Colors.grey.shade400,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-      child: Padding(
-        padding: const EdgeInsets.all(12),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // Image
-            Expanded(child: Image.asset(item.imageUrl)),
-
-            // Name
-            Text(item.itemName,
-            style: const TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold
-            ),),
-            
-            // Price
-            Row(
+    return Stack(
+      children: [
+        Card(
+          elevation: 1,
+          shadowColor: Colors.grey.shade400,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                Text(item.price.toString(),
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: CustomColors.customSwatchColor
-                  ),),
+                // Image
+                Expanded(child: Image.asset(item.imageUrl)),
 
-                // Unit
-                Text('/${item.unit}',
-                  style: TextStyle(
-                      color: Colors.grey.shade400,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 12
-                  ),)
+                // Name
+                Text(
+                  item.itemName,
+                  style: const TextStyle(
+                      fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+
+                // Price
+                Row(
+                  children: [
+                    Text(
+                      utilsServices.priceToCurrency(item.price),
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: CustomColors.customSwatchColor),
+                    ),
+
+                    // Unit
+                    Text(
+                      '/${item.unit}',
+                      style: TextStyle(
+                          color: Colors.grey.shade400,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12),
+                    )
+                  ],
+                )
               ],
-            )
-          ],
+            ),
+          ),
         ),
-      ),
+        Positioned(
+          top: 4,
+          right: 4,
+          child: GestureDetector(
+            onTap: () {},
+            child: Container(
+              decoration: BoxDecoration(
+                  color: CustomColors.customSwatchColor,
+                  borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(15),
+                      topRight: Radius.circular(20))),
+              height: 40,
+              width: 35,
+              child: const Icon(
+                Icons.add_shopping_cart_outlined,
+                color: Colors.white,
+                size: 26,
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
---
+++ lib/src/services/utils_services.dart
@@ -0,0 +1,11 @@
+// frozen_string_literal: true
+
+import 'package:intl/intl.dart';
+
+class UtilsServices {
+
+  String priceToCurrency(double price) {
+    NumberFormat numberFormat = NumberFormat.simpleCurrency(locale: 'pt_BR',);
+    return numberFormat.format(price);
+  }
+}
