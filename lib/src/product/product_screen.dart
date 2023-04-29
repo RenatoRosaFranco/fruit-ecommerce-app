b3b159da1253a7f09a573048bdba655e9d70b7c9
Renato Rosa Franco <renato_ny@live.com>
2023-04-29 16:10:46 -0300

    Adicionando os dados do container

--- lib/src/product/product_screen.dart
+++ lib/src/product/product_screen.dart
@@ -1,12 +1,15 @@
 // frozen_string_literal: true
 
 import 'package:flutter/material.dart';
+import 'package:untitled4/src/config/custom_colors.dart';
 import 'package:untitled4/src/models/item_model.dart';
+import 'package:untitled4/src/services/utils_services.dart';
 
 class ProductScreen extends StatelessWidget {
-  const ProductScreen({Key? key, required this.item}) : super(key: key);
+  ProductScreen({Key? key, required this.item}) : super(key: key);
 
   final ItemModel item;
+  final UtilsServices utilsServices = UtilsServices();
 
   @override
   Widget build(BuildContext context) {
@@ -19,18 +22,63 @@ class ProductScreen extends StatelessWidget {
           ),
           Expanded(
             child: Container(
+              padding: const EdgeInsets.all(32),
               decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: const BorderRadius.vertical(
-                  top: Radius.circular(50),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey.shade600,
-                    offset: const Offset(0, 2)
-                  )
-                ]
-              )
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.vertical(
+                    top: Radius.circular(50),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                        color: Colors.grey.shade600, offset: const Offset(0, 2))
+                  ]),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          item.itemName,
+                          maxLines: 2,
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                              fontSize: 27, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Container(
+                        width: 70,
+                        height: 30,
+                        color: Colors.red,
+                      )
+                    ],
+                  ),
+
+                  // Price
+                  Text(
+                    utilsServices.priceToCurrency(item.price),
+                    style: TextStyle(
+                        fontSize: 23,
+                        fontWeight: FontWeight.bold,
+                        color: CustomColors.customSwatchColor),
+                  ),
+
+                  // Description
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: SingleChildScrollView(
+                          child: Text(
+                        item.description,
+                        style: const TextStyle(height: 1.5),
+                      )),
+                    ),
+                  ),
+
+                  // Button
+                  ElevatedButton(onPressed: () {}, child: const Text('Comprar'))
+                ],
+              ),
             ),
           )
         ],
