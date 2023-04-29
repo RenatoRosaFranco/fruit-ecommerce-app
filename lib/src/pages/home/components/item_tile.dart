01e50a479623477ef1c4887d552fa69778d74aec
Renato Rosa Franco <renato_ny@live.com>
2023-04-29 15:42:32 -0300

    Criando pagina do produto, adicionando a imagem de apresentação e o container

--- lib/src/pages/home/components/item_tile.dart
+++ lib/src/pages/home/components/item_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:untitled4/src/config/custom_colors.dart';
 import 'package:untitled4/src/models/item_model.dart';
+import 'package:untitled4/src/product/product_screen.dart';
 import 'package:untitled4/src/services/utils_services.dart';
 
 class ItemTile extends StatelessWidget {
@@ -13,48 +14,57 @@ class ItemTile extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Card(
-          elevation: 1,
-          shadowColor: Colors.grey.shade400,
-          shape:
-              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-          child: Padding(
-            padding: const EdgeInsets.all(12),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                // Image
-                Expanded(child: Image.asset(item.imageUrl)),
 
-                // Name
-                Text(
-                  item.itemName,
-                  style: const TextStyle(
-                      fontSize: 16, fontWeight: FontWeight.bold),
-                ),
+        // Conteudo
+        GestureDetector(
+          onTap: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+              return ProductScreen(item: item);
+            }));
+          },
+          child: Card(
+            elevation: 1,
+            shadowColor: Colors.grey.shade400,
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+            child: Padding(
+              padding: const EdgeInsets.all(12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  // Image
+                  Expanded(child: Image.asset(item.imageUrl)),
 
-                // Price
-                Row(
-                  children: [
-                    Text(
-                      utilsServices.priceToCurrency(item.price),
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: CustomColors.customSwatchColor),
-                    ),
+                  // Name
+                  Text(
+                    item.itemName,
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
 
-                    // Unit
-                    Text(
-                      '/${item.unit}',
-                      style: TextStyle(
-                          color: Colors.grey.shade400,
-                          fontWeight: FontWeight.bold,
-                          fontSize: 12),
-                    )
-                  ],
-                )
-              ],
+                  // Price
+                  Row(
+                    children: [
+                      Text(
+                        utilsServices.priceToCurrency(item.price),
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: CustomColors.customSwatchColor),
+                      ),
+
+                      // Unit
+                      Text(
+                        '/${item.unit}',
+                        style: TextStyle(
+                            color: Colors.grey.shade400,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 12),
+                      )
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),
