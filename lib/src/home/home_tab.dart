fbeacb51fb941376b40908c7e2fbfc49e663461e
Renato Rosa Franco <renato_ny@live.com>
2023-04-29 13:02:14 -0300

    Adicionado badge e campo de pesquisa para a home_tab

--- lib/src/home/home_tab.dart
+++ lib/src/home/home_tab.dart
@@ -21,20 +21,53 @@ class HomeTab extends StatelessWidget {
               style: TextStyle(color: CustomColors.customContrastColor)),
         ])),
         actions: [
-          IconButton(
-              onPressed: () {},
-              icon: Icon(
-                Icons.shopping_cart,
-                color: CustomColors.customSwatchColor,
-              ))
+          Padding(
+            padding: const EdgeInsets.only(top: 15, right: 15),
+            child: GestureDetector(
+              onTap: () {},
+              child: Badge(
+                  backgroundColor: CustomColors.customContrastColor,
+                  label: const Text('2', style: TextStyle(color: Colors.white)),
+                  child: Icon(
+                    Icons.shopping_cart,
+                    color: CustomColors.customSwatchColor,
+                  )),
+            ),
+          ),
         ],
       ),
 
-      // Pesquisa
+      body: Column(
+        children: [
+          // Pesquisa
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  isDense: true,
+                  hintText: 'Pesquise aqui...',
+                  hintStyle: TextStyle(
+                    color: Colors.grey.shade400,
+                    fontSize: 14
+                  ),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: CustomColors.customContrastColor,
+                    size: 21,
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(60),
+                      borderSide:
+                          const BorderSide(width: 0, style: BorderStyle.none))),
+            ),
+          )
+          // Categorias
 
-      // Categorias
-
-      // Grid
+          // Grid
+        ],
+      ),
     );
   }
 }
